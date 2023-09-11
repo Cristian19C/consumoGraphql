12f7341577f26b2a5c8e254a6282dd969ecbbc3f
Cristian <cristian.cordoba01@uptc.edu.co>
2023-09-10 21:38:46 -0500

    Readme y documento final

--- index.js
+++ index.js
@@ -10,21 +10,12 @@ app.use(express.urlencoded({extended:false}))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-
-
-
 //setting 
 
 app.set('PORT',process.env.PORT || 4000)
 app.set(express.v)
 
-// app.get('/', (req, res) => res.send(" word"))
-// console.log(__dirname);
-// app.get('/', (req, res) =>{
 
-//     res.sendFile(path.join(__dirname,'View/index.html'))
-    
-// })
 
 app.use('/', require('./routes/index'))
 // app.use('/about', require('./routes/about'))
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Taller GraphQL
+
+Desarrollo del consumo de la persistencia de una Api, haciendo uso de GraphQl.
+
+## Integrantes del equipo
+
+- Cristian David Córdoba Tibocha
+- Juan Camilo Vargas Cardenaz
+
+## Dependencias
+- axios: "^1.5.0"
+- ejs: "^3.1.9"
+- express: "^4.18.2"
+- path: "^0.12.7"
+
+## Despliegue 
+
+Puerto configurado por defecto 4000
